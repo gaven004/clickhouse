3364f4fdcc03436d67a82c69804f2dc776626aff
Gaven <gaven_yeung@163.com>
2022-11-07 11:05:42 +0800

    UPDATE: Model改用lombok标注

--- src/main/java/org/mybatis/generator/plugins/SpringDataAnnotationPlugin.java
+++ src/main/java/org/mybatis/generator/plugins/SpringDataAnnotationPlugin.java
@@ -7,6 +7,7 @@ import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
 public class SpringDataAnnotationPlugin extends PluginAdapter {
@@ -49,8 +50,36 @@ public class SpringDataAnnotationPlugin extends PluginAdapter {
         return true;
     }
 
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        // skip getter and setter
+        return false;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        // skip getter and setter
+        return false;
+    }
+
     protected void addTableAnnotation(TopLevelClass topLevelClass,
                                       IntrospectedTable introspectedTable) {
+        topLevelClass.addImportedType(
+                new FullyQualifiedJavaType("lombok.Data")); //$NON-NLS-1$
+        topLevelClass.addAnnotation("@Data");
+
+        topLevelClass.addImportedType(
+                new FullyQualifiedJavaType("lombok.NoArgsConstructor")); //$NON-NLS-1$
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+
+        topLevelClass.addImportedType(
+                new FullyQualifiedJavaType("lombok.AllArgsConstructor")); //$NON-NLS-1$
+        topLevelClass.addAnnotation("@AllArgsConstructor");
+
         topLevelClass.addImportedType(
                 new FullyQualifiedJavaType("org.springframework.data.relational.core.mapping.Table")); //$NON-NLS-1$
         topLevelClass.addAnnotation(String.format("@Table(\"%s\")", introspectedTable.getFullyQualifiedTableNameAtRuntime()));
